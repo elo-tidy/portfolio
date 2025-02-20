72c41f4317e24ce95b12920e7c0ec3090b8068d7
elo-tidy <197076178+elo-tidy@users.noreply.github.com>
2025-02-20 13:38:23 +0100

    Google Fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,35 +2,40 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: {
-    enabled: true
+    enabled: true,
   },
-  debug: true,  
-  css: ['~/assets/styles/app.css'],
+  debug: true,
+  css: ["~/assets/styles/app.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-  modules: [
-    "@nuxtjs/supabase",
-    "@nuxtjs/color-mode"
-  ],
+  modules: ["@nuxtjs/supabase", "@nuxtjs/color-mode", "@nuxtjs/google-fonts"],
   supabase: {
     redirect: false,
   },
   colorMode: {
-    preference: 'dark', // default value of $colorMode.preference
-    fallback: 'dark', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: 'mode-',
-    classSuffix: '',
-    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
-    storageKey: 'nuxt-color-mode',
+    preference: "dark", // default value of $colorMode.preference
+    fallback: "dark", // fallback value if not system preference found
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "mode-",
+    classSuffix: "",
+    storage: "localStorage", // or 'sessionStorage' or 'cookie'
+    storageKey: "nuxt-color-mode",
     // dataValue: 'theme'
-  }
-  
-})
+  },
+  googleFonts: {
+    families: {
+      "Open Sans": {
+        wght: [400, 600, 700],
+        ital: [400],
+      },
+      Salsa: {
+        wght: [400],
+      },
+    },
+  },
+});
