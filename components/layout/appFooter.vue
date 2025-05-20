4f48bb2d3768fa693ef032a9d8c8d07f8da4c2f0
elo-tidy <197076178+elo-tidy@users.noreply.github.com>
2025-05-20 13:59:26 +0200

    Layout | Footer

--- components/layout/appFooter.vue
+++ components/layout/appFooter.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
 <template>
-    <footer id="footer" role="contentinfo"></footer>
+    <footer id="footer" role="contentinfo">
+        <div class="container-bloc mx-auto">
+            <div id="back-to-top">
+                <p>
+                    <NuxtLink
+                        to="#main"
+                        class="top-btn"
+                        title="Retourner à l'accueil"
+                    >
+                        <span class="sr-only">Accueil</span>
+                    </NuxtLink>
+                </p>
+            </div>
+            <p>© Eloïse Lombard {{ currentYear }} - All rights reserved</p>
+        </div>
+    </footer>
 </template>
