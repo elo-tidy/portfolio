e5e30ea6fb676ec33175980e54ae3e7ed4afd6b3
elo-tidy <197076178+elo-tidy@users.noreply.github.com>
2025-09-30 10:49:20 +0200

    chore: Vercel analytics module

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,13 +1,15 @@
 <script lang="ts" setup>
 import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+import { Analytics } from "@vercel/analytics/nuxt";
 </script>
 <template>
-    <SpeedInsights />
     <div>
         <layoutAppHeader />
         <main id="main" role="main">
             <slot />
         </main>
         <LayoutAppFooter />
+        <SpeedInsights />
+        <Analytics />
     </div>
 </template>
